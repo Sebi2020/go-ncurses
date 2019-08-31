f9ebca2b983add86684a491ac1e4d507e047dc82
Sebi2020 <info@informatikonline.net>
2019-08-31 14:36:41 +0200

    Added Format-String parser
    Signed-off-by: Sebi2020 <info@informatikonline.net>

--- attr.go
+++ attr.go
@@ -6,8 +6,11 @@ package ncurses
 import (
 	// #include <curses.h>
 	"C"
-	// "fmt"
+	"fmt"
+	"io"
+	"errors"
 )
+
 type Attribute int
 const (
 	// Normal text
@@ -32,6 +35,23 @@ const (
 	AttrAltcharset  Attribute = C.A_ALTCHARSET
 )
 
+func (a Attribute) String() string {
+	switch(a) {
+	case AttrNormal:
+		return "AttrNomal"
+	case AttrBold:
+		return "AttrBold"
+	case AttrItalic:
+		return "AttrItalic"
+	case AttrUnderline:
+		return "AttrUnderline"
+	case AttrReversed:
+		return "AttrReversed"
+	default:
+		return fmt.Sprintf("Unkown (%x)",a)
+	}
+}
+
 // Sets char attributes for following output in Window w.
 func (w *Window) SetAttribute(att... Attribute) {
 	attSet := 0
@@ -45,4 +65,112 @@ func (w *Window) SetAttribute(att... Attribute) {
 		Value: attSet,
 	}
 	GetComChannel() <- com
+}
+
+type AttributeWriter struct{
+	w io.Writer
+}
+
+type attrParserState int
+
+const (
+	A_STATE_CHAR attrParserState = iota
+	A_STATE_MODIFIER
+	A_STATE_ESCAPE
+	A_STATE_FORMAT_FLAG
+)
+
+func (st attrParserState) String() string {
+	switch(st) {
+	case A_STATE_CHAR:
+		return "A_STATE_CHAR"
+	case A_STATE_MODIFIER:
+		return "A_STATE_MODIFIER"
+	case A_STATE_ESCAPE:
+		return "A_STATE_ESCAPE"
+	case A_STATE_FORMAT_FLAG:
+		return "A_STATE_FORMAT_FLAG"
+	}
+	return ""
+}
+type modList []rune
+func (m modList) contains(r rune) bool {
+	for _,v := range ([]rune)(m) {
+		if(v == r) {
+			return true
+		}
+	}
+	return false
+}
+
+var modifiers modList = modList{'*','_','-','~'}
+
+var modMapping map[rune] Attribute = map[rune] Attribute{
+	'*':AttrBold,
+	'~':AttrReversed,
+	'-':AttrItalic,
+	'_':AttrUnderline,
+}
+
+type attrOp struct {
+	Attr Attribute
+	Offset int
+	Add bool
+}
+func initOpenStates(m modList) map[rune]bool {
+	open := make(map[rune]bool)
+	for _,v := range m {
+		open[v] = true
+	}
+	return open
+}
+
+func ParseFormatStr(s string) (string,[]attrOp) {
+	STATE := A_STATE_CHAR
+	output := make([]rune,0,255)
+	input := ([]rune)(s)
+	ops := make([]attrOp,0,10)
+	Iidx,Oidx := 0,0
+	open := initOpenStates(modifiers)
+
+	for Iidx < len(input) {
+		v := input[Iidx]
+		switch STATE {
+			case A_STATE_CHAR:
+				if modifiers.contains(v) {
+					STATE = A_STATE_MODIFIER
+				} else {
+					output = append(output,v)
+					Oidx++
+				}
+				Iidx++
+			case A_STATE_MODIFIER:
+				if input[Iidx-1] == v {
+					STATE = A_STATE_ESCAPE
+				} else {
+					STATE = A_STATE_FORMAT_FLAG
+				}
+			case A_STATE_FORMAT_FLAG:
+				ops = append(ops,attrOp{
+					Attr: modMapping[input[Iidx-1]],
+					Offset: Oidx,
+					Add: open[input[Iidx-1]],
+				})
+				open[input[Iidx-1]] = !open[input[Iidx-1]]
+				STATE = A_STATE_CHAR
+			case A_STATE_ESCAPE:
+				output = append(output,v)
+				Iidx++
+				Oidx++
+				STATE = A_STATE_CHAR
+		}
+	}
+	return string(output),ops
+}
+
+func (aw *AttributeWriter) Write([] byte) (int,error) {
+	if !initialized {
+		return 0,errors.New("ncurses is not initialized!")
+	}
+	return 0,io.EOF
 }
